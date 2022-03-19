078f108a1c3df373a45fce09061b5ce713d3546f
lsj8367 <lsj8367@naver.com>
2022-03-19 12:55:14 +0900

    :sparkles: Initial Commit

---
+++ src/main/java/io/github/jasmin/JasminApplication.java
@@ -0,0 +1,13 @@
+package io.github.jasmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasminApplication.class, args);
+    }
+
+}
