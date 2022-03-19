109386b5fda2c083055b609701c3b152435cd661
lsj8367 <lsj8367@naver.com>
2022-03-20 01:50:50 +0900

    :sparkles: 기본 controller - service - repository 세팅

---
+++ src/main/java/io/github/jasmin/member/MemberController.java
@@ -0,0 +1,13 @@
+package io.github.jasmin.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+}
---
+++ src/main/java/io/github/jasmin/member/MemberService.java
@@ -0,0 +1,14 @@
+package io.github.jasmin.member;
+
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+}
