136a6b257d585dd798413294b4ec39f49a79c40e
lsj8367 <lsj8367@naver.com>
2022-03-21 19:19:01 +0900

    :sparkles: 멤버 CRUD 구현

--- src/main/java/io/github/jasmin/member/MemberController.java
+++ src/main/java/io/github/jasmin/member/MemberController.java
@@ -1,12 +1,23 @@
 package io.github.jasmin.member;
 
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
@@ -18,4 +29,33 @@ public class MemberController {
     public ResponseEntity<List<Member>> findAllMembers() {
         return ResponseEntity.ok(memberService.findAllMembers());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Member> findMember(@PathVariable("id")
+    @Min(value = 0, message = "id는 1보다 크거나 같아야 합니다.")
+    @NotNull(message = "id는 null일 수 없습니다.") final Long id) {
+        return ResponseEntity.ok(memberService.findByMemberIdx(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateMember(@PathVariable("id")
+    @Min(value = 0, message = "idx는 1보다 크거나 같아야 합니다.")
+    @NotNull(message = "idx는 null일 수 없습니다.") final Long id,
+        @Valid @RequestBody final MemberUpdateRequest request) {
+        memberService.updateMember(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Object> saveMember(@RequestBody final MemberCreateRequest request) {
+        memberService.saveMember(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteMember(@PathVariable("id") Long id) {
+        memberService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/io/github/jasmin/member/MemberService.java
+++ src/main/java/io/github/jasmin/member/MemberService.java
@@ -1,5 +1,6 @@
 package io.github.jasmin.member;
 
+import io.github.jasmin.exception.NotFoundException;
 import java.util.List;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,24 @@ public class MemberService {
 
     public List<Member> findAllMembers() {
         return memberRepository.findAll();
+    }
+
+    public Member findByMemberIdx(final long id) {
+        return memberRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("해당하는 멤버를 찾을 수 없습니다."));
+    }
+
+    public void updateMember(final Long id, final MemberUpdateRequest request) {
+        final Member member = findByMemberIdx(id);
+        member.updateMember(request);
+    }
+
+    public void saveMember(final MemberCreateRequest request) {
+        memberRepository.save(request.toEntity());
+    }
+
+    public void deleteById(final Long id) {
+        memberRepository.deleteById(id);
     }
 
 }
