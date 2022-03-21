910a2235b74372bb8764bcb4b8f988785475e062
lsj8367 <lsj8367@naver.com>
2022-03-21 15:50:05 +0900

    :sparkles: 찾을수 없을 때 예외 생성

---
+++ src/main/java/io/github/jasmin/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package io.github.jasmin.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(final String message) {
+        super(message);
+    }
+
+}
