a9bf943f063c4044521dae5d038f91dec025dfda
lsj8367 <lsj8367@naver.com>
2022-03-21 19:39:27 +0900

    :recycle: 예외 핸들러 400 -> 404로 변경

--- src/main/java/io/github/jasmin/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/io/github/jasmin/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import io.github.jasmin.exception.NotFoundException;
 import java.time.LocalDateTime;
 import javax.servlet.http.HttpServletRequest;
 import lombok.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,7 +15,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Object> errorResponse(NotFoundException e, HttpServletRequest request) {
-        return ResponseEntity.badRequest()
+        return ResponseEntity.status(NOT_FOUND)
             .body(ErrorResponse.of(NOT_FOUND.value(), NOT_FOUND.getReasonPhrase(),
                 e.getMessage(), request.getRequestURI(), LocalDateTime.now()));
     }
